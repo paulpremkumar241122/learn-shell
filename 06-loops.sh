eda639b9befdd64717328910fc5bc63647712c8d
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-25 01:52:43 +0530

    loops

--- 06-loops.sh
+++ 06-loops.sh
@@ -2,11 +2,12 @@ a=10
 while [ $a -gt 0 ]; do
   echo Hello World
   a=$(($a-1))
-  sleep 3
+  sleep 1
 done
 
-echo "--------------- end ---------------------"
+echo "----------------------------- end -------------------------------"
 
 for component in frontend mongodb catalogue redis user cart shipping payment ; do
   echo Creating component - $component
+  sleep 2
   done
