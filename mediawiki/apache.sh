8f75c8ab6505e6e567c076689e67714aa9b88e75
Paul Prem Kumar <paul.prem5@gmail.com>
2023-12-26 22:53:28 +0530

    mediawiki

---
+++ mediawiki/apache.sh
@@ -0,0 +1,9 @@
+cd /tmp
+url="https://releases.wikimedia.org/mediawiki/1.41/mediawiki-1.41.0.tar.gz"
+curl -O $url
+folder=$(echo $url | awk -F / '{print $6}' | sed 's/.tar.gz//')
+yum install httpd -y
+rm -rf /var/www/html/*
+cd /var/www/html
+tar -xf /tmp/mediawiki-1.41.0.tar.gz
+mv $folder mediawiki
